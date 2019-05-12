fc10bc2ccc732ea5e4d6e89cbed6bccf7e0f197b
Lab3 <32977377+ayoz4@users.noreply.github.com>
2019-05-12 22:52:36 +0700

    slow added

--- animate.js
+++ animate.js
@@ -3,6 +3,7 @@ var bs = 30;
 var bx, by;
 var xv, yv;
 var canvas, context;
+const exp = 25 / FPS;
 
 canvas = document.getElementById('outputCanvas');
 context = canvas.getContext('2d');
@@ -12,20 +13,35 @@ setInterval(update, 1000 / FPS);
 bx =  canvas.width / 2;
 by = canvas.height / 2;
 
-xv = Math.floor(Math.random() * 150 + 25) / FPS;
-yv  = Math.floor(Math.random() * 150 + 25) / FPS;
+xv = Math.floor(Math.random() * 150 + 100) / FPS;
+yv  = Math.floor(Math.random() * 150 + 100) / FPS;
 
-if (Math.floor(Math.random() * 2) == 0) {
-    xv = -xv;
-}
 
-if (Math.floor(Math.random() * 2) == 0) {
-    yv = -yv;
-}
+console.log('xv = ' + xv + " " + 'yv = ' + yv);
 
 function update() {
+    if (xv > 0) {
+        xv -= 0.0111;
+    }
+    if (xv < 0) {
+        xv +=0.0111;
+    }
+
+   if (yv > 0) {
+        yv -=0.0111;
+    }
+    if (yv < 0) {
+        yv +=0.0111;
+    }
+
     bx += xv;
     by += yv;
+
+    console.log(xv, yv);
+
+    /* if ( xv <= 0 && yv <= 0) {
+        xv = yv = 0;
+    } */
 
     if (bx - bs / 2 < 0 && xv < 0) {
         xv = -xv;
